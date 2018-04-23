a38af18340c54d9aee5deaf3d68dcee49c7358f6
Terry Kerr <t@xnr.ca>
2018-04-23 19:10:01 -0400

    remove debug message, unregister scrollback when not needed

--- js/util.js
+++ js/util.js
@@ -1,10 +1,11 @@
 let at_bottom = () => new Promise((resolve, reject) => {
     let check_scroll = () => {
-        console.log('checking scroll');
         let d = document.documentElement;
         let offset = d.scrollTop + window.innerHeight;
-        if (offset >= d.offsetHeight)
+        if (offset >= d.offsetHeight) {
             resolve('hit bottom');
+	        window.onscroll = undefined;	
+		}
     };
     // check if we're already at bottom
     check_scroll();
